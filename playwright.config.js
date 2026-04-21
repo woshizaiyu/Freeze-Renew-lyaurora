196e912da5374101ba285e0dfe0c05c3b6fac9f8
woshizaiyu <lds924494@gmail.com>
2026-04-21 13:25:01 +0800

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   testDir: './tests',
   testMatch: '**/*.spec.js',
   timeout: 300000,
-  retries: 2,
+  retries: 0, // <--- 将这里的 2 改为 0，彻底关闭重试机制
   use: {
     headless: true,
   },
